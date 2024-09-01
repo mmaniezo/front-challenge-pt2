c8a1f4fdd6b6b97d3ae8da383efa115457e7bc4c
caio <caio62858@gmail.com>
2024-08-31 23:58:29 -0300

    header

--- script.js
+++ script.js
@@ -9,29 +9,12 @@ toggleBtn.onclick = function () {
     const isOpen = menuDropDown.classList.contains('open');
     toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
 }
-
-const chk = document.getElementById('chk');
-
-chk.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
-})
-
-let count = 1;
-document.getElementById("radio1").checked = true;
-
-setInterval (function(){
-    nextImage();
-}, 5000)
-
-function nextImage(){
-    count++;
-    if(count>3){
-        count = 1
+window.onresize = function () {
+    if (window.innerWidth > 1240) {
+        menuDropDown.classList.remove('open');
+        toggleBtnIcon.classList = 'fa-solid fa-bars';
     }
-    document.getElementById("radio"+count).checked = true;
-}
-
-
+};
 
 document.addEventListener('DOMContentLoaded', function() {
 
